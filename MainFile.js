f16874aa1e0cfa63006eabfeb96037c3aea76f29
Muthu <muthukumaran41998@gmail.com>
2023-02-23 10:36:00 +0530

    Updated CRUD function

--- MainFile.js
+++ MainFile.js
@@ -3,7 +3,9 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const {stringify} = require('uuid');
+const methodOverride = require('method-override')
 const Campground = require('./model/Templete');
+const { findByIdAndUpdate } = require('./model/Templete');
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelpcamp',{useNewUrlParser:true,useUnifiedTopology:true});
 const db = mongoose.connection;
@@ -12,17 +14,47 @@ db.once("open",()=>{
     console.log("Database connected");
 });
 
-
 app.set('views engine','ejs');
 app.set('views',path.join(__dirname,'views'))
 
-app.get('/yelpcamp',(req,res)=>{
-    res.render('mainview.ejs')
+app.use(express.urlencoded({extended:true}))
+app.use(methodOverride('_method'))
+
+app.get('/yelpcamp',async(req,res)=>{
+    const allcampgrounds = await Campground.find({});
+    res.render('home.ejs',{allcampgrounds})
 })
 
-app.get('/yelpcamp/templete',async(req,res)=>{
-    const camp = await new Campground({title:'My background',description:'Cheap camping!!'});
-    res.send(camp);
+app.get('/yelpcamp/new',(req,res)=>{
+    res.render('New.ejs')
+})
+app.post('/yelpcamp',async(req,res)=>{
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/yelpcamp/${campground._id}`) 
+})
+
+app.get('/yelpcamp/:id',async(req,res)=>{
+    const{id} =req.params;
+    const singleCamp = await Campground.findById(id)
+    res.render('show.ejs',{singleCamp});
+})
+
+app.get('/yelpcamp/:id/edit',async(req,res)=>{
+    const{id} =req.params;
+    const singleCamp = await Campground.findById(id)
+    res.render('edit.ejs',{singleCamp});
+})
+app.put('/yelpcamp/:id',async(req,res)=>{
+    const{id} = req.params;
+    const singleCamp= await Campground.findByIdAndUpdate(id,{...req.body.campground});
+    res.redirect(`/yelpcamp/${singleCamp._id}`) 
+})
+
+app.delete('/yelpcamp/:id',async(req,res)=>{
+    const{id}=req.params;
+    const deletecampground= await Campground.findByIdAndDelete(id);
+    res.redirect('/yelpcamp');
 })
 
 app.listen(3000,()=>{
--- seeds/index.js
+++ seeds/index.js
@@ -15,7 +15,7 @@ const sample = array => array[Math.floor(Math.random()*array.length)];
 
 const seedDB = async()=>{
     await Campground.deleteMany({});
-    for(let i=0;i<50;i++){
+    for(let i=0;i<20;i++){
         const random1000 = Math.floor(Math.random()*1000);
         const camp = new Campground({
             location:`${cities[random1000].city},${cities[random1000].state}`,
